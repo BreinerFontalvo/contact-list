77ea4eff2d55f09f0b95ee539571a01ce64a2f02
BreinerFontalvo <fontalvob71@gmail.com>
2022-11-24 11:14:06 -0500

    mi lista de contacto

---
+++ index.js
@@ -0,0 +1,36 @@
+let newContacto = "";
+let lista = ["jose", "henry", "geral"];
+let seguir = "";
+do {
+do {
+    newContacto = prompt("Agregar Nuevo Contactos Nombre y Apellido");
+    if (newContacto) {
+    add(newContacto, lista);
+    document.write(lista);
+    } else {
+    alert("ingresa tu nombre y apellido");
+    }
+} while (newContacto == "");
+let lista2 = "";
+for (let i = 0; i < lista.length; i++) {
+    lista2 += i + "_" + lista[i] + "  ";
+}
+let eliminar = prompt("Quiere Eliminar algun Contacto Agregado \n si o no");
+if (eliminar === "si") {
+    const Contacto = prompt(
+    "escribe el numero del contacto que quieres eliminar\n" + lista2 + "\n"
+    );
+    delet(Number(Contacto), lista);
+    document.write("Estos Son Tus Contacto"+lista);
+} else {
+    alert("No Eliminaste A Nadien De Tu Costactos");
+}
+function add(nombre, arr) {
+    arr.push(nombre);
+}
+function delet(eliminarContacto, arr) {
+    arr.splice(eliminarContacto, 1);
+}
+seguir = prompt("Quieres Seguir agregado \n si o no ");
+document.write("Estos Son Tus Contacto"+lista);
+} while (seguir === "si");
